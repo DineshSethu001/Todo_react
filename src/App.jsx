e1d0d86799ece9c14d91274bcad2eeecc543ee9a
DineshSethu001 <dineshsethu15981@gmail.com>
2023-11-11 11:34:25 +0530

    skeleton

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import InputBox from "./components/InputBox";
+import ListContainer from "./components/ListContainer";
+import "./App.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Todo App</h1>
+      <InputBox todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <ListContainer list={todos} remove={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
